867e87721c3f6ad45c3d37401dab002217784fc0
Vadym Frolov <vdfroloff@gmail.com>
2022-08-18 19:23:54 +0300

    upds

---
+++ src/components/ Routes/index.js
@@ -0,0 +1,5 @@
+export { default as PublicRoute } from './PublicRoute';
+export { default as PrivateRoute } from './PrivateRoute';
+
+// later will be
+// import {PublicRoute, PrivateRoute} from '../Routes'
