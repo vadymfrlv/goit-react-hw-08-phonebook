d46bdb485bd5907089bc3cd56fd933660854c894
Vadym Frolov <vdfroloff@gmail.com>
2022-08-19 21:57:32 +0300

    upds

--- src/redux/authorization/authorization-selectors.js
+++ src/redux/authorization/authorization-selectors.js
@@ -1,3 +1,3 @@
 export const getUserEmail = state => state.user.email;
 export const getUserToken = state => state.user.token;
-export const getLoggedIn = state => state.auth.isLoggedIn;
+export const getLoggedIn = state => state.user.isLoggedIn;
