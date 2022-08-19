2a280d32e35a4476552f37378df539c66f3b1a55
Vadym Frolov <vdfroloff@gmail.com>
2022-08-19 20:58:12 +0300

    upds

---
+++ src/components/UserMenu/UserMenu.jsx
@@ -0,0 +1,22 @@
+import { useDispatch, useSelector } from 'react-redux';
+import { userLogoutThunk } from 'redux/authorization/authorization-operations';
+import { getUserEmail } from 'redux/auth/authSelectors';
+import avatar from '/images/avatar.png';
+import styles from './UserMenu.module.css';
+
+const UserMenu = () => {
+  const userEmail = useSelector(getUserEmail);
+  const dispatch = useDispatch();
+
+  return (
+    <div className={styles.userMenu}>
+      <p className={styles.email}>{userEmail}</p>
+      <button className={styles.btn} type="button" onClick={() => dispatch(userLogoutThunk())}>
+        Logout
+      </button>
+      <img className={styles.avatar} src={avatar} width="30" height="30" alt="default avatar" />
+    </div>
+  );
+};
+
+export default UserMenu;
