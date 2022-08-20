02e68a1212ffacc13318a1fa340c15ccfb502c81
vadymfrlv <65303465+vadymfrlv@users.noreply.github.com>
2022-08-20 20:50:16 +0000

    Deploying to gh-pages from @ vadymfrlv/goit-react-hw-08-phonebook@8cdd8f775d419e55e86ab74fc627d09a04ab08ae 🚀

---
+++ static/js/ContaHomePagectsPage.66390e1c.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgoit_react_hw_08_phonebook=self.webpackChunkgoit_react_hw_08_phonebook||[]).push([[828],{8674:function(e,o,n){n.r(o),n.d(o,{default:function(){return s}});var t="HomePage_container__uNu-e",_="HomePage_title__UFLA3",a="HomePage_subTitle__YoKZp",c=n(3329),s=function(){return(0,c.jsxs)("div",{className:t,children:[(0,c.jsx)("p",{className:a,children:"Welcome to the"}),(0,c.jsx)("h1",{className:_,children:"Phonebook"})]})}}}]);
+//# sourceMappingURL=ContaHomePagectsPage.66390e1c.chunk.js.map
